bb3a1ae2b53c2d8a4d14a099a7e3baaf880282d6
Mor Elmeduey <mor.elmeduey@live.biu.ac.il>
2020-06-01 17:27:18 +0300

    Added Unit Tests

---
+++ FlightControlWebTests/FlightControlManagerTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FlightControlWeb.Models;
+
+namespace FlightControlWebTests
+{
+    [TestClass]
+    public class FlightControlManagerTests
+    {
+        [TestMethod]
+        public void add_get_removeServer_leagalInput_ReturnValidAnswers()
+        {
+            var actualLen = 0;
+            var expectedLen = 0;
+            var flightCtrlManager = new FlightControlManager();
+            var dummyServer = new Server();
+            dummyServer.ServerId = "testName";
+            dummyServer.ServerURL = "https://www.testUrl.com";
+            flightCtrlManager.addServer(dummyServer);
+            var result = flightCtrlManager.GetServers();
+            foreach (var item in result)
+            {
+                Assert.AreEqual(dummyServer.ServerId, item.ServerId);
+                Assert.AreEqual(dummyServer.ServerURL, item.ServerURL);
+            }
+            flightCtrlManager.RemoveServer(dummyServer.ServerId);
+            result = flightCtrlManager.GetServers();
+            foreach (var item in result)
+            {
+                actualLen++;
+            }
+            Assert.AreEqual(expectedLen, actualLen);
+        }
+
+        [TestMethod]
+        public void add_removeServer_eleagalInput_ReturnsOneItem()
+        {
+            var actualLen = 0;
+            var expectedLen = 1;
+            var wrongID = "wringID";
+            var flightCtrlManager = new FlightControlManager();
+            var dummyServer = new Server();
+            dummyServer.ServerId = "testName";
+            dummyServer.ServerURL = "https://www.testUrl.com";
+            flightCtrlManager.addServer(dummyServer);
+            var result = flightCtrlManager.GetServers();
+            flightCtrlManager.RemoveServer(wrongID);
+            result = flightCtrlManager.GetServers();
+            foreach (var item in result)
+            {
+                actualLen++;
+            }
+            Assert.AreEqual(expectedLen, actualLen);
+        }
+    }
+}
--- FlightControlWebTests/FlightPlanConrollerTests.cs
+++ FlightControlWebTests/FlightPlanConrollerTests.cs
@@ -4,6 +4,13 @@ using System;
 using FlightControlWeb.Models;
 using FlightControlWeb.Controllers;
 using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using FlightControlWeb.Models;
+using FlightControlWeb.Controllers;
+using System.Collections.Generic;
+using Autofac.Extras.Moq;
+using System.Threading.Tasks;
 
 namespace FlightControlWebTests
 {
@@ -39,7 +46,7 @@ namespace FlightControlWebTests
             var flightPlanTester = new FlightPlanController(new FlightControlManager());
              
             // Act
-            var result = flightPlanTester.IsValidFlightPlan(flightPlan);
+            var result = flightPlan.IsValidFlightPlan();
             // Assert
             Assert.IsTrue(result);
         }
@@ -50,9 +57,58 @@ namespace FlightControlWebTests
             // Arrange
             var flightPlanTester = new FlightPlanController(new FlightControlManager());
             // Act
-            var result = flightPlanTester.IsValidFlightPlan(new FlightPlan());
+            var result = new FlightPlan().IsValidFlightPlan();
             // Assert
             Assert.IsFalse(result);
+        }
+
+
+        [TestMethod]
+        public void GetPlanByIDTest_ValidData_ReturnsFlightPlan()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                var testID = "12345";
+                mock.Mock<IFlightManager>()
+                    .Setup(x => x.GetFlightPlan(testID))
+                    .Returns(getSampleFlightPlan(testID));
+
+                var controllerTester = mock.Create<FlightPlanController>();
+
+                var expected = getSampleFlightPlan(testID);
+
+                var actual = controllerTester.GetFlightPlan(testID);
+
+                Assert.IsTrue(actual != null);
+                Assert.AreEqual(expected.Result.Flight_Id, actual.Result.Flight_Id);
+                Assert.AreEqual(expected.Result.Company_Name, actual.Result.Company_Name);
+                Assert.AreEqual(expected.Result.Passengers, actual.Result.Passengers);
+            }
+        }
+
+        private async Task<FlightPlan> getSampleFlightPlan(string id)
+        {
+            var flightPlan = new FlightPlan();
+
+            //new segment list.
+            List<Segment> list = new List<Segment>();
+            var segments = new Segment();
+            segments.Latitude = 31.12;
+            segments.Longitude = 33.16;
+            segments.Timespan_seconds = 500;
+            list.Add(segments);
+            //new location.
+            var location = new LocationAndTime(16, 14, new DateTime());
+
+            //initial our flightPlan
+            //flightPlan.Flight_Id = "test12";
+            flightPlan.Flight_Id = id;
+            flightPlan.Passengers = 50;
+            flightPlan.Company_Name = "comp-name";
+            flightPlan.Initial_Location = location;
+            flightPlan.Segments = list;
+
+            return flightPlan;
         }
 
 
